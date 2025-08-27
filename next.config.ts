5096d18350eae5bfba35e0dfa8563f1bd1be3591
carlos rubio <carloviloria0@gmail.com>
2025-08-26 23:20:25 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     },
     {
       protocol: "https",
-      hostname: "mi-backend.com",
+      hostname: "clean-print-dashboard.onrender.com",
       pathname: "/uploads/**",
     },
   ],
